a6fccc2401f970d68fb6aed576e48ca9f3604116
liuyuan <373005226@qq.com>
2022-04-18 00:26:19 +0800

    添加vue模板

---
+++ microApp/micro-app-vite/src/page/Home/Home.tsx
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author ly
+ * @date 2021-09-15 23:26
+ * @since 0.0.0
+ */
+import { defineComponent } from 'vue'
+// import classnames from 'classnames'
+// import style from './style.module.scss'
+
+export default defineComponent({
+    setup() {
+        return {}
+    },
+    render() {
+        return <div>Home</div>
+    },
+})
